8faa65acb268f0f9caaaa8106f5c733b55fb8807
davanegasg <68928488+davanegasg@users.noreply.github.com>
2020-08-24 22:17:09 -0500

    Create noX.java

---
+++ laboratorios/lab01/ejercicioEnLinea/noX.java
@@ -0,0 +1,15 @@
+public class noX
+{
+    public String noX(String str) {
+    int i = 0;
+    if(str.equals("")){
+    return str;
+    }
+    if (str.charAt(i)=='x'){
+      return noX(str.substring(i+1));
+    } else {
+      return str.charAt(i) + noX(str.substring(i+1));
+    }
+  
+    }
+}
