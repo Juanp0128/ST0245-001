91d7da53160819fa60b476a9699696352868e024
Juanp0128 <66357966+Juanp0128@users.noreply.github.com>
2020-09-29 03:44:24 -0500

    Create Arco.java

---
+++ laboratorios/lab03/codigo/Arco.java
@@ -0,0 +1,13 @@
+package Laboratorio03;
+
+public class Arco {
+    Conexion conexion;
+    String distance;
+    String name;
+
+    public Arco(Conexion conexion, String distance, String name) {
+        this.conexion = conexion;
+        this.distance = distance;
+        this.name = name;
+    }
+}
