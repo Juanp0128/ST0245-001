8b129c67e83612305f5ccddcd3428890c3502536
Juanp0128 <66357966+Juanp0128@users.noreply.github.com>
2020-09-29 03:31:18 -0500

    Create Triplet.java

---
+++ laboratorios/lab03/codigo/Triplet.java
@@ -0,0 +1,13 @@
+package Laboratorio03;
+
+public class Triplet<X, Y, Z> {
+    public final X x;
+    public final Y y;
+    public final Z z;
+
+    public Triplet(X x, Y y, Z z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
