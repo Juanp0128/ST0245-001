fc1529cfb468977a70f36bce4f2719b6882a7424
Juanp0128 <66357966+Juanp0128@users.noreply.github.com>
2020-10-18 23:44:10 -0500

    Create Node.java

---
+++ talleres/taller10/Node.java
@@ -0,0 +1,12 @@
+package Tall10;
+
+public class Node {
+    public Node left,rigth;
+    public int data;
+
+        public Node(int data) {
+            this.left = null;
+            this.rigth = null;
+            this.data = data;
+        }
+    }
