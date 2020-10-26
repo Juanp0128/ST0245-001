4034ebc4f41dccabef6fd9c26c0f6bc6f80bfc83
Juanp0128 <66357966+Juanp0128@users.noreply.github.com>
2020-10-26 01:32:55 -0500

    Create Graph.java

---
+++ talleres/taller11/Graph.java
@@ -0,0 +1,25 @@
+package Taller11;
+import java.util.ArrayList;
+
+
+public abstract class Graph {
+    protected int size;
+    public int Vertices;
+    public int Arcos;
+
+    public Graph(int vertices) {
+        size = vertices;
+    }
+
+    public abstract void addArc(int source, int destination, int weight);
+
+    public abstract ArrayList<Integer> getSuccessors(int vertex);
+
+   
+    public abstract int getWeight(int source, int destination);
+
+
+    public int size() {
+        return size;
+    }
+}
