db75d40fd367007dc01c2e7e01f359451f79772e
davanegasg <68928488+davanegasg@users.noreply.github.com>
2020-10-25 23:13:48 -0500

    Create Bee.java

---
+++ laboratorios/lab04/codigo/Bee.java
@@ -0,0 +1,35 @@
+public class Bee
+{
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    public Bee (double latitude, double longitude, double altitude){
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.altitude=altitude;
+    }
+    
+    /**
+     * Returns the latitude
+     * @return latitude
+     */
+    public double getLatitude(){
+        return latitude;
+    }
+    
+    /**
+     * Returns the longitude
+     * @return longitude
+     */
+    public double getLongitude(){
+        return longitude;
+    }
+    
+    /**
+     * Returns the altitude
+     * @return altitude
+     */
+    public double getAltitude(){
+        return altitude;
+    }
+}
