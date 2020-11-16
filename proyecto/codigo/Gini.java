7e79428bd9adc71c282bf99617b572508c93364c
Juanp0128 <66357966+Juanp0128@users.noreply.github.com>
2020-11-16 03:11:31 -0500

    Se agrega otro algoritmo de impureza Gini
    Mirar cual es mas eficiente y quedarnos con uno

--- proyecto/codigo/Gini.java
+++ proyecto/codigo/Gini.java
@@ -1,4 +1,5 @@
 package Proyectoo;
+import java.util.TreeSet;
 //Juan Pablo Henao, Diego Vanegas
 public class Gini {
     private String variable;
@@ -43,23 +44,102 @@ public class Gini {
         noNode0++;
     }
 
-    public void impurezaGini(){
-        float allyes = yesNode0+yesNode1;
-        float allno= noNode0+noNode1;
-        float V0yes, V1yes,V0no,V1no,ginisi,ginino,giniV;
+    public void impurezaGini() {
+        float allyes = yesNode0 + yesNode1;
+        float allno = noNode0 + noNode1;
+        float V0yes, V1yes, V0no, V1no, ginisi, ginino, giniV;
 
-        V0yes = yesNode0/allyes;
-        V1yes = yesNode1/allyes;
-        V0no = noNode0/allno;
-        V1no = noNode1/allno;
-        ginisi = 1-((V0yes*V0yes)+(V1yes*V1yes));
-        ginino = 1-((V0no*V0no)+(V1no*V1no));
-        giniV= ((allno*ginino)+(allyes*ginisi))/(allno+allyes);
-        this.index=giniV;
+        V0yes = yesNode0 / allyes;
+        V1yes = yesNode1 / allyes;
+        V0no = noNode0 / allno;
+        V1no = noNode1 / allno;
+        ginisi = 1 - ((V0yes * V0yes) + (V1yes * V1yes));
+        ginino = 1 - ((V0no * V0no) + (V1no * V1no));
+        giniV = ((allno * ginino) + (allyes * ginisi)) / (allno + allyes);
+        this.index = giniV;
 
-        System.out.println(variable+"="+value+"Impureza Gini: "+giniV);
+        System.out.println(variable + "=" + value + "Impureza Gini: " + giniV);
     }
+
+    public static TreeSet<String> valueSet(String[][] matrix, int pos) {
+        TreeSet<String> values = new TreeSet<String>();
+        for (int i = 0; i < matrix.length; i++) {
+            values.add(matrix[i][pos]);
+        }
+        return values;
+    }
+//Se implementa un algoritmo distinto para analizar cual es mas eficiente
+    public static float academicGini(int pos, String[][] matrix, String value) {
+        float ganaRight = 0;
+        float ganaLeft = 0;
+        float pierdeRight = 0;
+        float pierdeLeft = 0;
+        float left = 0;
+        float right = 0;
+        if (Numerico(matrix[1][pos])) {
+            for (int i = 0; i < matrix.length; i++) {
+                if (Double.parseDouble(matrix[i][pos]) >= Double.parseDouble(value)) {
+                    left++;
+                    if (matrix[i][matrix[0].length - 1].equals("0")) {
+                        pierdeLeft++;
+                    } else {
+                        ganaLeft++;
+                    }
+
+                } else {
+                    right++;
+                    if (matrix[i][matrix[0].length - 1].equals("0")) {
+                        pierdeRight++;
+                    } else {
+                        ganaRight++;
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < matrix.length; i++) {
+                if (matrix[i][pos].equals(value)) {
+                    left++;
+                    if (matrix[i][matrix[0].length - 1].equals("0")) {
+                        pierdeLeft++;
+                    } else {
+                        ganaLeft++;
+                    }
+                } else {
+                    right++;
+                    if (matrix[i][matrix[0].length - 1].equals("0")) {
+                        pierdeRight++;
+                    } else {
+                        ganaRight++;
+                    }
+                }
+            }
+        }
+        if (right == 0) {
+            return (float) ((float) 1 - (Math.pow(pierdeLeft / left, 2) + (Math.pow(ganaLeft / left, 2))));
+        } else if (left == 0) {
+            return (float) ((float) 1 - (Math.pow(pierdeRight / right, 2) + (Math.pow(ganaRight / right, 2))));
+        } else {
+            float academicLeft = (float) ((float) 1 - (Math.pow(pierdeLeft / left, 2) + (Math.pow(ganaLeft / left, 2))));
+            float academicRight = (float) ((float) 1 - (Math.pow(pierdeRight / right, 2) + (Math.pow(ganaRight / right, 2))));
+            float academicPon = (float) (((academicLeft * left) + (academicRight * right)) / (right + left));
+            return academicPon;
+        }
+    }
+
+    public static boolean Numerico(String num){
+        if (num==null){
+            return false;
+        }
+        try {
+            double n = Double.parseDouble(num);
+        }catch (NumberFormatException FFF){
+            return false;
+        }
+        return true;
+    }
+
     public float getIndex() {
         return index;
     }
 }
+
