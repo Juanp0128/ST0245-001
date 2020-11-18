c0aee213e2a25f19bf19539375820eb54fe18e06
Juanp0128 <66357966+Juanp0128@users.noreply.github.com>
2020-11-18 14:26:39 -0500

    Update Tree.java

--- proyecto/codigo/Tree.java
+++ proyecto/codigo/Tree.java
@@ -5,25 +5,31 @@ public class Tree {
     private String name;
     private TreeSet<String> variableCollector = new TreeSet<String>();
     private Gini [] Allvariables;
+    int i;
 
-    public Tree(String name) {
+    public Tree(String name,int i){
         this.name = name;
+        this.i = i;
     }
+
 
     public void addVariable(String Variable) {
-        this.variableCollector.add(Variable);
+        variableCollector.add(Variable);
     }
 
-    public Gini[] getAllvariables() {
-        return Allvariables;
+    public String getName(){
+        return name;
     }
 
     public void FillVariables(){
         Allvariables = new Gini[variableCollector.size()];
         int i=0;
         for (String V:variableCollector) {
-            Allvariables[i]=new Gini(V,name);
+            Allvariables[i]=new Gini(V,name,i);
             i++;
         }
+    }
+    public Gini[] getAllvariables() {
+        return Allvariables;
     }
 }
